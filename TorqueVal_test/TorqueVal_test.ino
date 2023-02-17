63cb5e0a15d3be4f044282884e4d596d43e3caf9
Konfi <t20me022@yamanashi.ac.jp>
2023-02-17 16:44:03 +0900

    PST-360G2用のコード

--- TorqueVal_test/TorqueVal_test.ino
+++ TorqueVal_test/TorqueVal_test.ino
@@ -9,9 +9,12 @@ void setup() {
 
 void loop() {
   Val = analogRead(INPUT_PIN);
-  
-  float a = 120.0 / 1023.0;  //傾き,120:最大トルク60の2倍
-  int y = a * Val;             //トルク範囲に置き換える0～120
+  float AnaMin = 1023.0 * 0.1;          //出力関数：0°＝10%
+  float AnaMax = 1023.0 * 0.9;          //出力関数：360°＝90%
+  float a = 120.0 / (AnaMax - AnaMin);  //傾き,120:最大トルク60の2倍
+  float b = -a * AnaMin;
+
+  int y = a * Val + b;     //トルク範囲に置き換える0～120
   float Torque = 0.5 * y;  //トルク値, 0～60
 
   Serial.print("Val: ");
