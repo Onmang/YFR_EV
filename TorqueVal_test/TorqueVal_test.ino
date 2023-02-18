d51fa457ba80004da6af0173cd7c2bc72791309e
Cong_Huy <t20me022@yamanashi.ac.jp>
2023-02-19 02:19:11 +0900

    CAN送信コードを追加 ，※0.5刻みで送信できない

--- TorqueVal_test/TorqueVal_test.ino
+++ TorqueVal_test/TorqueVal_test.ino
@@ -1,13 +1,22 @@
-//ポテションメータのトルク値変換
-
+//ポテションメータのトルク値変換&送信
+#include <mcp_can.h>
+#include <SPI.h>
+MCP_CAN CAN(10);
 const int INPUT_PIN = 0;  //アナログ入力ピン
 int Val = 0;
 
 void setup() {
   Serial.begin(9600);
+  if (CAN.begin(MCP_ANY, CAN_500KBPS, MCP_8MHZ) == CAN_OK) {
+    CAN.setMode(MCP_NORMAL);
+  } else {
+    Serial.println("Can init fail");
+  }
 }
 
 void loop() {
+  static byte buf[] = { B00000001, 0, 0, 0, 0, 0, 0, 0 };  //0byte目はMG-ECU:on, Co放電要求:off 固定
+
   Val = analogRead(INPUT_PIN);
   float AnaMin = 1023.0 * 0.1;          //出力関数：0°＝10%
   float AnaMax = 1023.0 * 0.9;          //出力関数：360°＝90%
@@ -16,7 +25,20 @@ void loop() {
 
   int y = a * Val + b;     //トルク範囲に置き換える0～120
   float Torque = 0.5 * y;  //トルク値, 0～60
+  buf[1] = Torque;
 
+  //CAN.sendMsgBuf(0x301, 0, 8, buf);
+
+  unsigned char sndStat = CAN.sendMsgBuf(0x301, 0, 8, buf);  //ID:0x02, 標準フレーム:0, データ長:8
+  if (sndStat == CAN_OK) {
+    Serial.print("CANsend:");
+    Serial.print(Torque);
+    Serial.print(" [Nm]");
+  } else {
+    Serial.print("Error Sending Message...");
+  }
+
+  Serial.print("  ||  ");  //これ以降は参照値，コメントアウトしてOK
   Serial.print("Val: ");
   Serial.print(Val);
   Serial.print("   y:");
