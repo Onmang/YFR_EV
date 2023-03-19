f41397ae1f68940532c28d594e4efff3a37878ca
Cong_Huy <t20me022@yamanashi.ac.jp>
2023-03-20 01:03:49 +0900

    precharge指令pin→「３」に変更

--- Main_code_Cpu1/Main_code_Cpu1.ino
+++ Main_code_Cpu1/Main_code_Cpu1.ino
@@ -13,7 +13,7 @@ MCP_CAN CAN(10);                      //CAN通信ポート
 LiquidCrystal lcd(9, 8, 7, 6, 5, 4);  //LiquidCrystal(rs,enable, d4, d5, d6, d7)
 
 const int IGNSW_PIN = 2;      //IGNSWのデジタル入力ピン
-const int Precharge_PIN = 4;  //Precharge制御マイコンのデジタル入力ピン
+const int Precharge_PIN = 3;  //Precharge制御マイコンのデジタル入力ピン
 
 const int APPS_PIN = 0;        //APPS,アナログ入力ピン
 int val = 0;                   //APPS,アナログ入力の変数
