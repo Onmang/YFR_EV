2a128a52802aadf27807fc7428d0e6121c4c1ea1
Mochizuki_Takushi <t21jm046@yamanashi.ac.jp>
2023-04-21 00:03:53 +0900

    無負荷試験　容易に編集した

--- Sub_code_Cpu2/Sub_code_Cpu2.ino
+++ Sub_code_Cpu2/Sub_code_Cpu2.ino
@@ -7,18 +7,21 @@ const int IGNSW_PIN = 2;     //IGNSWのデジタル入力ピン,割り込み
 const int AIR_PIN = 7;       //AIR+の制御ピン
 const int PRErelay_PIN = 8;  //precharge relayの制御ピン
 const int PREsend_PIN = 4;   //CPU1につなげるピン
-int Presta = 0;  //precharge実行したかを判断する, 1:実行済み，0：未実行
+int Presta = 0;              //precharge実行したかを判断する, 1:実行済み，0：未実行
 int TS = 0;
 
-void PreStop(void);  //割り込み関数
+//void PreStop(void);  //割り込み関数
 
 void setup() {
+  /*
+  Serial.begin(9600);
+  */
   pinMode(IGNSW_PIN, INPUT);
   pinMode(AIR_PIN, OUTPUT);
   pinMode(PRErelay_PIN, OUTPUT);
   pinMode(PREsend_PIN, OUTPUT);
 
-  attachInterrupt(0, PreStop, FALLING);
+  //attachInterrupt(0, PreStop, FALLING);
 
   digitalWrite(PRErelay_PIN, LOW);
   digitalWrite(AIR_PIN, LOW);
@@ -39,16 +42,30 @@ void loop() {
       digitalWrite(PREsend_PIN, HIGH);  //Cpu1に送信
       Presta = 1;
     }
+
   } else if (TS == LOW) {
     digitalWrite(PRErelay_PIN, LOW);
     digitalWrite(AIR_PIN, LOW);
     digitalWrite(PREsend_PIN, LOW);
     Presta = 0;
   }
-}  //loop終了
+/*
+  Serial.print("Presta: ");
+  Serial.print(Presta);
 
+
+  Serial.print(" ; precharge realy: ");
+  Serial.print(PRErelay_PIN);
+
+  Serial.print(" ; TS: ");
+  Serial.println(TS);
+*/
+}  //loop終了
+/*   
 void PreStop(void) {
   digitalWrite(PRErelay_PIN, LOW);
   digitalWrite(AIR_PIN, LOW);
   digitalWrite(PREsend_PIN, LOW);
+  Presta = 0;
 }
+*/
